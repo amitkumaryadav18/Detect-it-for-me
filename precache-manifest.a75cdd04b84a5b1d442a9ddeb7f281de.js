0d2d71f9fdde7a49fe81e155bcb1abfdae3318b3
Amit Kumar Yadav <amitky123@gmail.com>
2019-11-03 17:28:13 +0530

    Updates

---
+++ precache-manifest.a75cdd04b84a5b1d442a9ddeb7f281de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b34a65b942e0eb79af868b33c48ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "877a4f755a0afaf9c042",
+    "url": "/static/css/main.8e532cc5.chunk.css"
+  },
+  {
+    "revision": "552f8e6902865a089028",
+    "url": "/static/js/2.30ef76d3.chunk.js"
+  },
+  {
+    "revision": "877a4f755a0afaf9c042",
+    "url": "/static/js/main.3f2b46d4.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);
